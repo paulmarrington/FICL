247a0467d3c53e2f92c70719dbfe56514687846f
Paul Marrington <paul@marrington.net>
2014-02-06 15:10:52 +1000

    documentation updates

--- java/ficl.java
+++ java/ficl.java
@@ -8,18 +8,21 @@ public class ficl {
   public static void main(String[] args) {
     try {
       final FICL ficl = new FICL();
+      // Called to exit the REPL
       ficl.extend("_exit_", new Runnable() {
         public void run() {
           System.out.println("FICL repl exiting");
           System.exit(ficl.popInt());
         }
       });
+      // Used by uSDLC2 to end an instrumentation session
       ficl.extend("_end_", new Runnable() {
         public void run() {
           System.out.println("Errors: " + errors);
           errors = 0;
         }
       });
+      // Used by uSDLC2 to check results of a test
       ficl.extend("expected", new Runnable() {
         public void run() {
           Object expected = ficl.pop();
@@ -33,11 +36,11 @@ public class ficl {
           }
         }
       });
-
-      //ficl.throwExceptions = false;
+      // REPL
       BufferedReader bufferRead = new BufferedReader(
         new InputStreamReader(System.in));
       while (true) {
+        // *R*ead
         String s = bufferRead.readLine();
         if (s == null) {
           System.out.println(
@@ -47,16 +50,19 @@ public class ficl {
         if (s.length() > 0 && s.charAt(0) != '_') {
           System.out.print(s);
         }
+        // *E*xecute
         ficl.run(s);
         if (ficl.context.abort) {
           System.out.println("\nError: " +
             ficl.context.errors.toString());
           errors++;
         } else {
+          // *P*rint
           System.out.println(ficl.toString());
         }
         System.out.flush();
         System.err.flush();
+        // *L*oop
       }
     } catch (IOException e) {
       System.out.println(e.toString());
